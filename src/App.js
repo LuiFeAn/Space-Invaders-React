b27d4c915503ba2212f8aed1a6a4adc0a872c2e1
luis <luisfernandogvv@gmail.com>
2022-11-11 11:50:58 -0300

    Implementando Score no componente App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Nave from "./components/Nave";
+import { ScoreContainer } from "./appStyles";
 import GlobalStyles from "./assets/styles/globalStyles";
+import { useState } from "react";
 
 export default function App(){
+
+  const [ score, setScore ] = useState(0);
+
   return (
     <>
+      <ScoreContainer>
+        <span>
+          {`SCORE: ${score}`}
+        </span>
+      </ScoreContainer>
       <GlobalStyles/>
       <Nave/>
     </>
---
+++ src/appStyles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const ScoreContainer = styled.div`
+
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: flex-end;
+
+    span{
+        font-size:30px;
+        color:white;
+        margin: 50px;
+        font-weight: bold;
+        font-family: 'VT323', monospace;
+    }
+
+`;
