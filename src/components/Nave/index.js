62730cfdc2c249a125994bfd44371eaf8be06409
luis <luisfernandogvv@gmail.com>
2022-11-11 09:48:43 -0300

    Inicializando projeto, criando arquitetura de pastas e excluindo alguns arquivos

---
+++ src/components/Nave/index.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import * as S from './style';
+
+export default function Nave (){
+
+    const [ hp, setHp ] = useState(200);
+    const [ velocity, setVelocity ] = useState(100);
+    const [ attack, setAttack ] = useState(50);
+    
+    
+
+}
---
+++ src/components/Nave/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const NaveStyleImage = styled.img`
+
+    width: 120px;
+    height:120px;
+    position:absolute;
+    bottom: 10px;
+    
+`;
