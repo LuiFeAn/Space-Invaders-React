0ec2fc444e06d3d052f954d8fffc14366b2f364d
luis <luisfernandogvv@gmail.com>
2022-11-12 19:55:16 -0300

    Movendo regra de "Pause Game" para o componente Modal e removendo o observador de evento keypress ao componente ser desmontado

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import * as S from './style';
 import  ReactPortal  from 'react-dom';
 import Button from '../Button';
@@ -7,6 +8,18 @@ export default function Modal({title,titleTwo, onGameStart, changeGameStart}){
     const handleGameStart = () => changeGameStart(true);
 
     const quitGame = () => window.close();
+
+    useEffect( () => {
+
+        document.addEventListener('keypress', (event) => {
+            if(event.key === 'Enter'){
+              changeGameStart(false);
+            }
+        })
+
+        return ()=> document.removeEventListener('keypress', ()=> null);
+
+    },[]);
 
     return ReactPortal.createPortal(
         <S.ModalContainer>
